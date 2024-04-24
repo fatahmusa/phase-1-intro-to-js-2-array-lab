2a26fdb666d1558e12ec53bbd91b46d9a6a839ac
Abdifatah Musa <lawznleytha@gmail.com>
2024-04-24 23:26:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 // Write your solution here!
+var cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph){
+
+    cats.push("Ralph");
+}
+
+beforeEach(function (){
+    cats.length = 0
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyPrependCat(Bob){
+    cats.unshift("Bob");
+}
+
+beforeEach(function (){
+    cats.length = 0
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop("Garfield");
+}
+
+
+beforeEach(function (){
+    cats.length = 0
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyRemoveFirstCat(){
+    cats.shift("Milo");
+}
+
+beforeEach(function (){
+    cats.length = 0
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function appendCat(Broom){
+  return appendCat = [...cats, "Broom" ];
+}
+
+
+beforeEach(function (){
+    cats.length = 0
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function prependCat(Arnold){
+    return appendCat = ["Arnold", ...cats ];
+}
+
+beforeEach(function (){
+    cats.length = 0
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function removeLastCat(){
+    return cats.slice(0,cats.length - 1);
+}
+
+beforeEach(function (){
+    cats.length = 0
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
